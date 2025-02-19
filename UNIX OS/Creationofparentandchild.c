0d9319fad4280debe940723dd8e461709a700fda
Padilam Jayasri <jayasri73737@gmail.com>
2025-02-19 11:57:38 +0530

    Create Creationofparentandchild.c

---
+++ UNIX OS/Creationofparentandchild.c
@@ -0,0 +1,26 @@
+    // 2) Write programs using the following UNIX operating system calls for fork, getpid
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>  // Required for exit()
+
+int main() 
+{
+    int pid, pid1, pid2;
+    
+    pid = fork();  // Create a child process
+    
+    if (pid == -1) {
+        printf("Error in process creation\n");
+        exit(1);  // Exit if fork fails
+    }
+    
+    if (pid != 0) {  // Parent process
+        pid1 = getpid();
+        printf("\nThe parent process ID is %d\n", pid1);
+    } else {  // Child process
+        pid2 = getpid();
+        printf("\nThe child process ID is %d\n", pid2);
+    }
+
+    return 0;
+}
